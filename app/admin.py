24ecdf52d6cad8f65aff4f8cec762dac13583c11
camiloac98 <camiloalbertoc98@gmail.com>
2022-09-11 14:58:46 -0500

    Creando Administrador y anexando la BD

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from app.models import *
+
+admin.site.register(rol)
+admin.site.register(empresa)
+admin.site.register(estado_financiero)
+admin.site.register(usuario)
+admin.site.register(registro_contable)
+admin.site.register(empleado)
