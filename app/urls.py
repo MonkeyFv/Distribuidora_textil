fdd6c5e0ce40872b1d7f05b9ce900784c9d9a0e1
camiloac98 <camiloalbertoc98@gmail.com>
2022-09-13 22:52:24 -0500

    Arreglando un error

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,6 @@ from app.views import *
 urlpatterns=[
     path('Empresas/',empresaView.as_view(),name="Listado de Empresas"),
     path('Empresas/<str:NIT>',empresaView.as_view(),name="Busqueda de Empresas"),
-    path('Empleado',empleadoView.as_view(),name="Listado Empleados"),
+    path('Empleado/',empleadoView.as_view(),name="Listado Empleados"),
     path('Empleado/<int:ID>',empleadoView.as_view(),name="Buscar Empleados")
 ]
